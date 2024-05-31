7c90464e0e64f9df4f6720a921e0f084576020b7
samurainisans <samurainisa4@gmail.com>
2024-06-01 00:12:00 +0700

    update: change cors settings backend

--- MasterClassAPI/settings.py
+++ MasterClassAPI/settings.py
@@ -44,10 +44,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://localhost:5174',
+]
 ROOT_URLCONF = 'MasterClassAPI.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
